209a70201be14ee6e03e84e216484e53c598a8ed
Edbeer <edbeermtn@gmail.com>
2021-10-20 02:56:18 +0700

    product detail

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from .views import *
 app_name = 'store'
 
 urlpatterns = [
-    path('', all_products, name='all_products')
+    path('', all_products, name='all_products'),
+    path('item/<slug:slug>/', product_detail, name='product_detail'),
 ]
