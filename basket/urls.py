307855340b457604c0c65c785bab256cb23e1b67
Edbeer <edbeermtn@gmail.com>
2021-10-24 03:18:23 +0700

    fix url and session

--- basket/urls.py
+++ basket/urls.py
@@ -5,5 +5,5 @@ app_name = 'basket'
 
 urlpatterns = [
     path('', basket_summary, name='basket_summary'),
-    path('', basket_add, name='basket_add')
+    path('add/', basket_add, name='basket_add')
 ]
