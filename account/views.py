2f8d70b0cd9155da7f015aff83ced48fb2187ddb
Edbeer <edbeermtn@gmail.com>
2021-10-28 16:54:50 +0700

    register 1 step

--- account/views.py
+++ account/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import RegistrationForm
+
+
+def account_register(request):
+
+    if request.user.is_authenticated():
+        return redirect('/')
+
+    if request.method == 'POST':
+        register_form = RegistrationForm(request.POST)
+        if register_form.is_valid():
+            user = register_form.save(commit=False)
+            user.email = register_form.cleaned_data['email']
+            user.set_password(register_form.cleaned_data['password'])
+            user.is_active = False
+            user.save()
