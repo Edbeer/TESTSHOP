9fe39a34e764fb67f2e17a564bfe51ce3796845b
Edbeer <edbeermtn@gmail.com>
2021-11-11 18:00:42 +0700

    basket update delivery

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,7 +6,7 @@ app_name = 'checkout'
 
 urlpatterns = [
     path('delivery_choices/', delivery_choices, name='delivery_choices'),
-    # path('basket_update_delivery/', basket_update_delivery, name='basket_update_delivery'),
+    path('basket_update_delivery/', basket_update_delivery, name='basket_update_delivery'),
     # path('delivery_address/', delivery_address, name='delivery_address'),
     # path('payment_selection/', payment_selection, name='payment_selection'),
     # path('payment_complete/', payment_complete, name='payment_complete'),
