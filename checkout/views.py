a81dd7a5690604335d66b9b43514443cb277796a
Edbeer <edbeermtn@gmail.com>
2021-11-11 19:25:54 +0700

    address page

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.contrib import messages
+from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 from basket.basket import Basket
+from account.models import Address
+
 from .models import DeliveryOptions
 
 
@@ -32,3 +35,22 @@ def basket_update_delivery(request):
         response = JsonResponse({'total': updated_total_price,
                                  'delivery_price': delivery_type.delivery_price})
         return response
+
+
+@login_required
+def delivery_address(request):
+    session = request.session
+    if 'purchase' not in request.session:
+        messages.success(request, 'Please select delivery option')
+        # HTTP_REFERER - предыдущий урл
+        return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+    addresses = Address.objects.filter(customer=request.user).order_by("-default")
+
+    if 'address' not in request.session:
+        session['address'] = {'address_id': str(addresses[0].id)}
+    else:
+        session['address']['address_id'] = str(addresses[0].id)
+        session.modified = True
+
+    return render(request, 'checkout/delivery_address.html', {'addresses': addresses})
